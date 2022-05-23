1986e8cbf5bbd32172b528cb9cc171ceb3c01e60
illia841 <illiaholodniak@gmail.com>
2022-05-23 19:58:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'OTU1NTI2NjYyNjg1MDg5ODU1.G1jYpu.CYLflaUeEL1hXfbQdq0cCtBoc69vQ_VTNFRfTQ',
-    'bot': 'GuardBot',
-    'id': 955526662685089855,
-    'prefix': 'spam!'
+    'token': 'bot token',
+    'bot': 'bot name',
+    'id': bot id,
+    'prefix': 's!'
 } 
