01669c61750aecdb16eeb71c4873dc68c9eff6f7
Illia <59759522+illia841@users.noreply.github.com>
2022-05-23 19:54:16 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'OTU1NTI2NjYyNjg1MDg5ODU1.G1jYpu.CYLflaUeEL1hXfbQdq0cCtBoc69vQ_VTNFRfTQ',
-    'bot': 'GuardBot',
-    'id': 955526662685089855,
-    'prefix': 'spam!'
-} 
